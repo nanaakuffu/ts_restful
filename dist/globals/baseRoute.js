7a7806813925f827f96000a20d077760c364dbd4
nanaakuffu <nakuffu@aims.ac.tz>
2021-09-19 23:03:10 +0000

    removed transpiled files

--- dist/globals/baseRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseRoute = void 0;
-class BaseRoute {
-}
-exports.BaseRoute = BaseRoute;
-//# sourceMappingURL=baseRoute.js.map
